cb36091449fd8db2f06523f7df490279677af217
developerMahian <developermahian@gmail.com>
2022-03-19 15:34:28 +0600

    hidden api keys on github

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-	"x-rapidapi-host": "coinranking1.p.rapidapi.com",
-	"x-rapidapi-key": "f5e153c0c6msh29f2bdb7028b126p1fe0f5jsn3fa25ffd5423",
+	"x-rapidapi-host": process.env.REACT_APP_API_HOST,
+	"x-rapidapi-key": process.env.REACT_APP_API_KEY,
 };
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
 	reducerPath: "cryptoApi",
-	baseQuery: fetchBaseQuery({ baseUrl: "https://coinranking1.p.rapidapi.com" }),
+	baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
 	endpoints: (builder) => ({
 		getCryptos: builder.query({
 			query: (cardCount) => createRequest(`/coins?limit=${cardCount}`),
